6a9c6af6058de8e2fd78b141d497bb2f3087580c
guerrerosoconm@gmail.com <Vespertino@C09PC01.informatica.comercio>
2022-02-09 21:25:13 +0100

    Primera prueba unitaria (Eliminar Alumnno)

---
+++ ED_repaso_bloque2-master/src/com/iescomercio/ed/bloque2/repaso/modelo/CursoTest.java
@@ -0,0 +1,53 @@
+package com.iescomercio.ed.bloque2.repaso.modelo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.Test;
+
+
+
+class CursoTest {
+
+	@Test
+	void testEliminarAlumno() {
+		Persona p1 = new Persona("123AB","Marcos","Guerreros");
+		Persona p2 = new Persona("246BC", "Carlos", "Adan");
+		Curso c = new Curso();
+				
+		c.aniadirAlumno(p1);
+		c.aniadirAlumno(p2);
+
+		Exception e = assertThrows(Exception.class, ()->c.eliminarAlumno("123F"));
+		assertEquals(e.getMessage(), "El dni no tiene la longitud adecuada");
+		
+
+	}
+
+	@Test
+	void testAniadirAlumno() {
+	
+	}
+
+	@Test
+	void testEstaRegistrado() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCurso() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testNumeroAlumnos() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testMostrarTodos() {
+		fail("Not yet implemented");
+	}
+
+}
