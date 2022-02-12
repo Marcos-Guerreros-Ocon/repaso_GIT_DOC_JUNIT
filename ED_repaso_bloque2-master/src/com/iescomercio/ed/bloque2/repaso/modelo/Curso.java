5813f82e094595beadc295db22f58ef0359a579c
Marcos <guerrerosoconm@gmail.com>
2022-02-12 11:55:48 +0100

    Update Documentacion Curso.java

--- ED_repaso_bloque2-master/src/com/iescomercio/ed/bloque2/repaso/modelo/Curso.java
+++ ED_repaso_bloque2-master/src/com/iescomercio/ed/bloque2/repaso/modelo/Curso.java
@@ -4,59 +4,63 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Curso {
-	
+
 	private List<Persona> listaAlumnos;
 
-	/*
-	 * Elimina un alumno del curso por su numero del DNI
-	 * Para ello primero comprueba que la longitud del DNI es de 9 digitos
-	 * Si no lo es retorna un error
-	 * Si es de 9 digitos elimina el alumno
-	 * 
-	 * */
+	/**
+	 * Eliminar alumno del Curso
+	 * @param dni DNI del alumno que quieres eliminar
+	 * @throws Exception Si el DNI no tiene la longitud de 9 digitos
+	 */
 	public void eliminarAlumno(String dni) throws Exception {
-		if(dni.length()==9) {//comprobar la longitud del dni
-			listaAlumnos.remove(new Persona(dni, "", "")); //solo hace falta el dni
-		}else {
+		if (dni.length() == 9) {// comprobar la longitud del dni
+			listaAlumnos.remove(new Persona(dni, "", "")); // solo hace falta el dni
+		} else {
 			throw new Exception("El dni no tiene la longitud adecuada");
 		}
 	}
-	
-	/*
+
+	/**
 	 * Añade un alumno al curso
-	 * */
+	 * 
+	 * @param Objeto alumno a añadir
+	 */
 	public void aniadirAlumno(Persona p) {
 		listaAlumnos.add(p);
 	}
-	
-	/*
-	 * Devuelve true si el alumno esta en el curso y false en caso contrario
-	 * */
+
+	/**
+	 *  Nos dice si un alumno esta registrado en el curso
+	 * @param dni DNI del alumno que queremos saber si esta registrado en el curso
+	 * @return Retorna true si el alumno esta registrado y false en caso contrario
+	 */
 	public Boolean estaRegistrado(String dni) {
-		int i =0;
-		Boolean encontrado=false;
-		while (!encontrado && i<listaAlumnos.size()) {
-			if(listaAlumnos.get(i).getDni().equals(dni)) {
-				encontrado=true;
+		int i = 0;
+		Boolean encontrado = false;
+		while (!encontrado && i < listaAlumnos.size()) {
+			if (listaAlumnos.get(i).getDni().equals(dni)) {
+				encontrado = true;
 			}
 			i++;
 		}
 		return encontrado;
 	}
-	
-	/*
+
+	/**
 	 * Constructor de la clase Curso
-	 * */
+	 */
 	public Curso() {
-		listaAlumnos= new ArrayList<Persona>();
+		listaAlumnos = new ArrayList<Persona>();
 	}
-	
-	/*
-	 * Devuelve la cantidad de alumnos que hay en el curso
-	 * */
-	public Integer numeroAlumnos() {return listaAlumnos.size();}
-	
-	
+
+	/**
+	 *  Saber cuantos alumnos hay en un curso
+	 * @return El numero de alumnos que hay en un curso
+	 */
+	public Integer numeroAlumnos() {
+		return listaAlumnos.size();
+	}
+
 	public void mostrarTodos() {
 		for (Persona persona : listaAlumnos) {
 			System.out.println(persona);
